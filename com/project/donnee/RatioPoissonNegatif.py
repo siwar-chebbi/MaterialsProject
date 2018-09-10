e03c50ff464cbfa81694c5a8c66e1cc512c4904e
Mohamed <chebbimohamed1987@live.fr>
2018-09-10 06:17:40 +0200

    ajout colonne materiral_id

--- com/project/donnee/RatioPoissonNegatif.py
+++ com/project/donnee/RatioPoissonNegatif.py
@@ -1,17 +1,5 @@
-from pandas._libs import properties
 from pymatgen import MPRester
-import matplotlib.pyplot as plt
-import matplotlib.cm as cm
-import matplotlib.colors as color
-import os
-import matplotlib.backends.backend_pdf
 
-
-# from pymatgen.analysis.phase_diagram import PhaseDiagram, PDPlotter
-
-from pymatgen.analysis.elasticity import elastic
-from pymatgen.util import plotting
-import numpy as np
 
 api = MPRester("fB610TDF3LSwxiN9")
 
@@ -19,7 +7,7 @@ api = MPRester("fB610TDF3LSwxiN9")
 covalent = ['B', 'C', 'Si']
 ionique = ['N', 'O', 'F', 'P', 'S', 'Cl', 'Sr', 'Br', 'I']
 #tableau des proprietes
-propsTableau = ['pretty_formula','elasticity.poisson_ratio','elasticity.G_Reuss', 'elasticity.G_Voigt', 'elasticity.G_Voigt_Reuss_Hill', 'elasticity.K_Reuss', 'elasticity.K_Voigt', 'elasticity.K_Voigt_Reuss_Hill']
+propsTableau = ['material_id','pretty_formula','elasticity.poisson_ratio','elasticity.G_Reuss', 'elasticity.G_Voigt', 'elasticity.G_Voigt_Reuss_Hill', 'elasticity.K_Reuss', 'elasticity.K_Voigt', 'elasticity.K_Voigt_Reuss_Hill']
 
 critere1 = {"nelements": {'$lte': 6},'elements': {'$in': covalent}, "elasticity": {'$ne': None}, "elasticity.poisson_ratio": {'$lt': 0 },'elasticity.G_Reuss' : {'$gte': 0 }, 'elasticity.G_Voigt': {'$gte': 0 }, 'elasticity.G_Voigt_Reuss_Hill': {'$gte': 0 }, 'elasticity.K_Reuss': {'$gte': 0 }, 'elasticity.K_Voigt': {'$gte': 0 }, 'elasticity.K_Voigt_Reuss_Hill': {'$gte': 0 }}
 critere2 = {"nelements": {'$lte': 6},'elements': {'$in': ionique}, "elasticity": {'$ne': None}, "elasticity.poisson_ratio": {'$lt': 0 },'elasticity.G_Reuss' : {'$gte': 0 }, 'elasticity.G_Voigt': {'$gte': 0 }, 'elasticity.G_Voigt_Reuss_Hill': {'$gte': 0 }, 'elasticity.K_Reuss': {'$gte': 0 }, 'elasticity.K_Voigt': {'$gte': 0 }, 'elasticity.K_Voigt_Reuss_Hill': {'$gte': 0 }}
@@ -41,7 +29,7 @@ def recup(materials):
         print(texte)
 
 
-print("\n********************ELEEMENTS COVALENTS*******************************\n")
+print("\n********************ELEMENTS COVALENTS*******************************\n")
 recup(materials1)
-print("\n********************ELEEMENTS IONIQUES*******************************\n")
+print("\n********************ELEMENTS IONIQUES*******************************\n")
 recup(materials2)
