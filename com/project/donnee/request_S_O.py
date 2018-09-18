04b17a770c30538742080e003daedab806ed14c0
siwar-chebbi <siwar.chebbi@chimieparistech.psl.eu>
2018-09-18 12:07:23 +0200

    Request S and O

--- com/project/donnee/request_S_O.py
+++ com/project/donnee/request_S_O.py
@@ -3,7 +3,7 @@
 from pymatgen import MPRester
 
 api = MPRester("fB610TDF3LSwxiN9")
-results = api.query({"elements": {'$all': ['S', 'O']}, 'nelements': {'$lte': 6}, 'elasticity': {'$ne': None}}, properties=['pretty_formula', 'elasticity'])
+results = api.query({"elements": {'$all': ['S', 'O']}, "nelements": {'$lte': 6}, "elasticity": {'$ne': None}}, properties=["pretty_formula", "elasticity"])
 N = len(results)
 
 print("Le nombre d'éléments contenant S et O est : " + str(N))
